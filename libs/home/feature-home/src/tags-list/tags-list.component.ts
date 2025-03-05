7f227ebf0299fea5a96d63736e64c0aff4c227c9
NursultanFront <nursultan020395@gmail.com>
2025-03-05 21:35:48 +0500

    first branch commit

--- libs/home/feature-home/src/tags-list/tags-list.component.ts
+++ libs/home/feature-home/src/tags-list/tags-list.component.ts
@@ -1,11 +1,15 @@
+import { NgClass } from '@angular/common';
 import { Component, ChangeDetectionStrategy, output, input } from '@angular/core';
 
 @Component({
   selector: 'cdt-tags-list',
   templateUrl: './tags-list.component.html',
+  styleUrl: './tags-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgClass],
 })
 export class TagsListComponent {
   tags = input<string[]>([]);
+  selectedTags = input<string>();
   setListTag = output<string>();
 }
